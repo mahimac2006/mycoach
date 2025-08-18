2a037b157197333616e24bb095c60e9d4e61d714
Mahima Chaudhary <mcdiwa18@gmail.com>
2025-08-17 22:28:36 -0700

    move API keys to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,18 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBj-CNF_k8Mgh0fsQA8HTtDQcqv3w7Jd38",
-  authDomain: "ai-running-coach-c94e5.firebaseapp.com",
-  projectId: "ai-running-coach-c94e5",
-  storageBucket: "ai-running-coach-c94e5.firebasestorage.app",
-  messagingSenderId: "639352241332",
-  appId: "1:639352241332:web:8c0295dadc88951afb6be1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 const auth = getAuth(app);
 
